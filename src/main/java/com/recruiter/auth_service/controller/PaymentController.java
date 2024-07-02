09529fdcd6ee538808875e4abec9e5ef0af8c579
M ARJUN RAMESHAN <marjunr666@gmail.com>
2024-07-02 17:33:41 +0530

    subscription

---
+++ src/main/java/com/recruiter/auth_service/controller/PaymentController.java
@@ -0,0 +1,9 @@
+package com.recruiter.auth_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/payment")
+public class PaymentController {
+}
